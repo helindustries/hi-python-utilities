3bcd64f3dd01a666eec38f8268e01098c9cdbbd7
Paul Schulze <pschulze@helindustries.com>
2025-05-04 21:36:00 +0200

    Update copyrights

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-#  Copyright 2023 Hel Industries, all rights reserved.
+#  Copyright 2023-2025 $author, All rights reserved.
 #
 #  For licensing terms, Please find the licensing terms in the closest
 #  LICENSE.txt in this repository file going up the directory tree.
